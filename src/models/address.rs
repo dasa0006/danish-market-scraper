2e161104550dab9b94916541a9f7ee0f2bdc9a3d
daniman123 <daniel-saleh@hotmail.com>
2026-02-20 21:05:33 +0100

    refactor: reorganized api response data structure structs

--- src/models/address.rs
+++ src/models/address.rs
@@ -1,6 +1,9 @@
 use serde::{Deserialize, Serialize};
 
-use crate::models::Links;
+use crate::models::{
+    Links,
+    location::{City, Coordinates, Municipality, Province, Road, Zip},
+};
 
 /// An address entry.
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -104,92 +107,6 @@ pub struct Building {
     pub total_area: Option<i64>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub year_built: Option<i64>,
-}
-
-/// City information.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct City {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub slug: Option<String>,
-}
-
-/// Municipality details.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Municipality {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub church_tax_percentage: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub council_tax_percentage: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub land_value_tax_level_per_thousand: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub municipality_code: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub number_of_schools: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub population: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub slug: Option<String>,
-}
-
-/// Province information.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Province {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub province_code: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub region_code: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub slug: Option<String>,
-}
-
-/// Road details.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Road {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub municipality_code: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub road_code: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub road_id: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub slug: Option<String>,
-}
-
-/// Zip code information.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Zip {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub slug: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub zip_code: Option<i64>,
-}
-
-/// Geographical coordinates.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Coordinates {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub lat: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub lon: Option<f64>,
-    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
-    pub coord_type: Option<String>,
 }
 
 /// A registration record.
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,10 +1,9 @@
 pub mod address;
 pub mod api_response;
 pub mod links;
+pub mod location;
 
-pub use address::{
-    Address, Building, City, Coordinates, DaysOnMarket, Municipality, Province, Registration, Road,
-    Zip,
-};
+pub use address::{Address, Building, DaysOnMarket, Registration};
 pub use api_response::ApiResponse;
 pub use links::{Links, SelfLink};
+pub use location::{City, Coordinates, Municipality, Province, Road, Zip};
